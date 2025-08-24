edda0138dd7fe552e5e9a2a2b4264fcd1ee02ebe
jacobschvaneveldt <jacobschvaneveldt@gmail.com>
2025-08-24 10:42:04 -0600

    test

---
+++ keymap.c
@@ -0,0 +1,153 @@
+#include QMK_KEYBOARD_H
+#include <cyboard.h>
+#include "oneshot.h"
+
+enum layers {
+  _ENTHIUM,
+//   _ENTHIUM_REVERSED,
+  _LOWER,
+  _RAISE,
+  _ADJUST,
+  _F_ROW,
+  _GAME,
+  _GAME_NUM
+};
+
+enum keycodes {
+    // Custom oneshot mod implementation with no timers.
+    OS_SHFT = SAFE_RANGE,
+    OS_CTRL,
+    OS_ALT,
+    OS_CMD
+};
+
+#define LOWER MO(_LOWER)
+#define RAISE MO(_RAISE)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    //   z y u o ; q l d p x
+    // w c i e a , k h t n s f
+    //   ' - = . / j m g b v
+    //         r
+    [_ENTHIUM] = LAYOUT_fun(
+        KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                         KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12,
+        _______,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+        _______,  KC_Z,    KC_Y,    KC_U,    KC_O,    KC_SCLN,                       KC_Q,    KC_L,    KC_D,    KC_P,    KC_X,    _______,
+        KC_W,     KC_C,    KC_I,    KC_E,    KC_A,    KC_COMM,                       KC_K,    KC_H,    KC_T,    KC_N,    KC_S,    KC_F,
+        KC_LSFT,  KC_QUOT, KC_MINS, KC_EQL,  KC_DOT,  KC_SLSH,                       KC_J,    KC_M,    KC_G,    KC_B,    KC_V,    KC_RSFT,
+
+                       KC_LEFT, KC_RIGHT,   KC_R, OS_SHFT,  KC_ESC,           KC_LCMD, KC_BSPC, KC_SPC,  KC_DOWN, KC_UP,
+                                            LOWER, KC_LCTL, KC_TAB,           KC_LALT,  KC_ENT, RAISE
+    ),
+
+    //   x p d l q ; o u y z
+    // f s n t h k , a e i c w
+    //   v b g m j / . = - '
+    //         r
+
+    // [_ENTHIUM_REVERSED] = LAYOUT_fun(
+    //     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                               KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+    //     KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+    //     KC_TAB,  KC_X,    KC_P,    KC_D,    KC_L,    KC_Q,                                KC_SCLN, KC_O,    KC_U,    KC_Y,    KC_Z,    KC_BSLS,
+    //     KC_F,    KC_S,    KC_N,    KC_T,    KC_H,    KC_K,                                KC_COMM, KC_A,    KC_E,    KC_I,    KC_C,    KC_W,
+    //     KC_LSFT, KC_V,    KC_B,    KC_G,    KC_M,    KC_J,                                KC_SLSH, KC_DOT,  KC_EQL,  KC_MINS, KC_QUOT, KC_RSFT,
+
+    //                       KC_LGUI, KC_LALT, KC_R,  KC_DEL,  KC_ESC,           KC_GRAVE, KC_BSPC, KC_SPC,  KC_LBRC, KC_RBRC,
+    //                                         KC_LGUI, KC_ENT, KC_END,           KC_CAPS,  KC_APP, LOWER
+    // ),
+
+    [_LOWER] = LAYOUT_fun(
+        QK_BOOT, _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______,  _______,
+        _______, _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______,  _______,
+        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     _______,
+        _______, OS_CTRL, OS_ALT,  OS_CMD,  OS_SHFT, KC_PERC,                  KC_ESC,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,  _______,
+        _______, KC_MPRV, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY,                  KC_DEL,  KC_BSPC, KC_TAB,  KC_ENT,  KC_SCLN,  _______,
+
+                          _______, _______, _______, _______, _______,         _______, _______, _______, _______, _______,
+                                            _______, _______, _______,         _______, _______, _______
+    ),
+
+    [_RAISE] = LAYOUT_fun(
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+        _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_TILD,                           KC_CIRC, KC_AMPR, KC_QUES, KC_SLSH, KC_ASTR, _______,
+        _______, KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, KC_PERC,                           KC_PIPE, OS_SHFT, OS_CMD,  OS_ALT,  OS_CTRL, _______,
+        _______, KC_LBRC, KC_RBRC, KC_UNDS, KC_MINS, KC_GRV,                            KC_BSLS, KC_EQL,  KC_RABK, KC_LABK, KC_PLUS, _______,
+
+                          _______, _______, KC_SPC,  _______, _______,         _______, _______, _______, _______, _______,
+                                            _______, _______, _______,         _______, _______, _______
+    ),
+
+    [9] = LAYOUT_fun(
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+                          _______, _______, _______, _______, _______,         _______, _______, _______, _______, _______,
+                                            _______, _______, _______,         _______, _______, _______
+    )
+};
+
+
+__attribute__((weak)) combo_t key_combos[] = {
+
+};
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
+
+// one shot cancel
+bool is_oneshot_cancel_key(uint16_t keycode) {
+    switch (keycode) {
+    case LOWER:
+    case RAISE:
+        return true;
+    default:
+        return false;
+    }
+}
+
+bool is_oneshot_ignored_key(uint16_t keycode) {
+    switch (keycode) {
+    case LOWER:
+    case RAISE:
+    case KC_LSFT:
+    case OS_SHFT:
+    case OS_CTRL:
+    case OS_ALT:
+    case OS_CMD:
+        return true;
+    default:
+        return false;
+    }
+}
+
+oneshot_state os_shft_state = os_up_unqueued;
+oneshot_state os_ctrl_state = os_up_unqueued;
+oneshot_state os_alt_state = os_up_unqueued;
+oneshot_state os_cmd_state = os_up_unqueued;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    update_oneshot(
+        &os_shft_state, KC_LSFT, OS_SHFT,
+        keycode, record
+    );
+    update_oneshot(
+        &os_ctrl_state, KC_LCTL, OS_CTRL,
+        keycode, record
+    );
+    update_oneshot(
+        &os_alt_state, KC_LALT, OS_ALT,
+        keycode, record
+    );
+        update_oneshot(
+        &os_cmd_state, KC_LCMD, OS_CMD,
+        keycode, record
+    );
+
+    return true;
+}
+// end of one shot cancel
