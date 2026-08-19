5a1297bd15042c57cfeb1d9f62787aad7ccf3bac
jacobschvaneveldt <jacobschvaneveldt@gmail.com>
2026-08-19 16:33:39 -0600

    r to space

--- keymap.c
+++ keymap.c
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,   KC_LSFT, KC_A,  KC_S,  KC_D,  KC_F,        KC_G,  KC_H,  KC_J,  KC_K,    KC_L,   KC_SCLN,
         _______,   KC_LCTL, KC_Z,  KC_X,  KC_C,  KC_V,        KC_B,  KC_N,  KC_M,  KC_COMM, KC_DOT, KC_SLSH,
 
-             KC_LEFT, KC_RIGHT, KC_R, OS_SHFT, KC_BSPC,       KC_ENT, KC_ESC, KC_SPC, KC_DOWN, KC_UP,
+           KC_LEFT, KC_RIGHT, KC_SPC, OS_SHFT, KC_BSPC,       KC_ENT, KC_ESC, KC_SPC, KC_DOWN, KC_UP,
                                 LOWER, KC_TAB, KC_LCMD,       KC_LALT, KC_LCTL, RAISE
     ),
 
